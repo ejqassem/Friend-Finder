a6d415b693026cab6840dacfac825aa52b6012f0
ejqassem <eyadqassem@Eyads-MacBook-Air.local>
2017-05-17 18:51:22 -0400

    added form validation

--- app/public/assets/app.js
+++ app/public/assets/app.js
@@ -4,6 +4,11 @@ $("#submit-form").on("click", function(event) {
   var goodUrl = false;
   var goodName = false;
 
+  $("#fixAll").empty();
+  $("#fixAll2").empty();
+  $("#fixName").empty();
+  $("#fixUrl").empty();
+
   // creating an add function for .reduce
   function add(a, b) {
     return parseInt(a) + parseInt(b);
@@ -16,25 +21,23 @@ $("#submit-form").on("click", function(event) {
   // function to validate whether the user-inputted image url contains proper url components
   function urlValidation(link) {
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expression);
     if ((link === "") || (link === null)) {
       goodUrl = false;
       return false;
     }
-    else if(link.match(regex)) {
+    else if(expression.test(link)) {
       alert("good url");
       goodUrl = true;
     }
   }
 
   function validateName(id) {
-    var expression = /^[a-zA-Z_]+$/;
-    var regex = new RegExp(expression);
+    var expression = /^[a-zA-Z0-9.'\-_\s]{1,20}$/;
     if((id === "") || (id === null)) {
       goodName = false;
       return false;
     }
-    else if (id.match(expression)) {
+    else if (expression.test(id)) {
       alert("good name");
       goodName = true;
     }
@@ -49,26 +52,39 @@ $("#submit-form").on("click", function(event) {
   // creating an array called userTotalScore with the total composite score from all users and storing them in userTotalScore
   // This is done via the .reduce function which iterates through each scores property to reduce it down to one element using the add function defined above passed as an argument
   var userTotalScore = answers.reduce(add, 0);
-  console.log("userTotalScore " + userTotalScore);
+
   var username = $("#username").val().trim();
 
   var photoLink = $("#photo-link").val().trim();
 
   // perform validation function calls after form values have been stored in variables
-  urlValidation(photoLink);
+  // fix alerts to append to page
   validateName(username);
+  urlValidation(photoLink);
   if ((goodName === false) && (goodUrl === false)) {
+    var newDiv = $("<div>");
+    newDiv.attr("id", "fixAll");
+    newDiv.html("<p> Please enter something in all fields!</p>");
+    $("#username-label").append(newDiv);
+    var newDiv2 = $("<div>");
+    newDiv2.attr("id", "fixAll2");
+    newDiv2.html("<p> Please enter something in all fields!</p>");
+    $("#photo-link-label").append(newDiv2);
     alert("Please enter something in all fields!");
     return false;
   }
   else if (goodName === false) {
-    alert("Please enter a name!");
-    console.log("bad name " + goodName);
+    var newDiv = $("<div>");
+    newDiv.attr("id", "fixName");
+    newDiv.html("<p> Please enter a valid name!</p>");
+    $("#username-label").append(newDiv);
     return false;
   }
   else if(goodUrl === false) {
-    alert("Please enter a url!");
-    console.log("bad url " + goodUrl);
+    var newDiv = $("<div>");
+    newDiv.attr("id", "fixUrl");
+    newDiv.html("<p> Please enter a valid url!</p>");
+    $("#photo-link-label").append(newDiv);
     return false;
   }
 
@@ -80,24 +96,23 @@ $("#submit-form").on("click", function(event) {
     "scores": answers
   };
 
-
+// making an AJAX request to /api/friends
   $.get("/api/friends", function(data) {
     // iterate through the returned userScores and save in array
     var potentialFriendArr = data.map(function(value) {
       // iterate on each user score and calculate their composite score via the reduce method
       return value.scores.reduce(add, 0);
     });
-    console.log("Potential Friend Arr" + potentialFriendArr);
 
     // iterate through the composite scores of listed users on server and save into new array
     var compositeArr = potentialFriendArr.map(function(value) {
       // subtract the composite scores from userTotalScore and return the absolute value and store inside new array(compositeArr)
       return Math.abs(value - userTotalScore);
     });
-    console.log("Composite Arr " + compositeArr);
+
     // using the array.slice method to make a copy of the composite arr before sorting the array
     var sortedCompositeArr = compositeArr.slice(0).sort(sortNum);
-    console.log("Sorted composite arr" + sortedCompositeArr);
+
     // storing the lowest value from sortedCompositeArr[0] in variable for reference;
     var lowestValue = sortedCompositeArr[0];
 
@@ -110,8 +125,6 @@ $("#submit-form").on("click", function(event) {
       }
     }
 
-
-    console.log("matched user index " + matchedUserIndex);
     var matchedUser = {
       name: data[matchedUserIndex].name,
       photo: data[matchedUserIndex].photo,
@@ -130,6 +143,8 @@ $("#submit-form").on("click", function(event) {
     // posting new user data /api/friends/
     $.post("/api/friends", newFriend).done(function(data){
       console.log("Adding new user to database");
+    }).fail(function(error) {
+      if(err) throw err;
     });
 
   });
