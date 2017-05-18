a48911fa59a71d717e4adff7d41ea7718d7a87cc
ejqassem <eyadqassem@Eyads-MacBook-Air.local>
2017-05-17 21:17:04 -0400

    updated 404 page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,12 @@ module.exports = function(app, path) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.use("/", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.status(404);
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
